57fc7cb2c7d1b80032c3cd8939582fd890f28437
htu <hturamoar@gmail.com>
2021-12-06 11:43:28 +0530

    Intial Commit
    Project Structure with dependencies, Spring MVC and Thymeleaf

---
+++ src/main/java/com/htu/htujokesapp/HtuJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.htu.htujokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtuJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtuJokesAppApplication.class, args);
+	}
+
+}
